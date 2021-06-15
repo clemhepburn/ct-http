7be9ed40963bff6db68f0007eb7e2cd2c89a22d1
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-15 12:05:37 -0700

    adjust GET and POST tests, write Boo POST, pass POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,28 +2,18 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it.only('uses GET to receive all from / ', async() => {
-    const content = {
-      body: 'hi',
-      status: '200 OK',
-      contentType: 'text/plain'
-    };
-
+  it('uses GET to receive all from / ', async() => {
     const res = await request(app)
       .get('/');
 
     expect(res.text).toEqual('hi');
   });
 
-  it.only('uses POST  to create a response from /echo', async() => {
+  it('uses POST  to create a response from /echo', async() => {
+    const res = await request(app)
+      .post('/echo');
 
-    const res = await request(app);
-
-    expect(res.text).toEqual(`HTTP/1.1 200 OK
-  Accept-Ranges: bytes
-  Content-Length: 0
-  Content-Type: text/plain\r
-  \r`);
+    expect(res.text).toEqual('Boo');
   });
 
 });
